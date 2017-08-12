d7814999a72f9fabd9c578184edf5cb98dc171f8
Francis <francis.screene@gmail.com>
2017-08-12 12:57:29 +0100

    Updated build script stuff

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+scp build/libs/airmart-shadow.jar aiko@164.132.230.218:/opt/airheadz/airmart/airmart.jar
+scp airmart.yml aiko@164.132.230.218:/opt/airheadz/airmart/airmart.yml
--- src/main/kotlin/xyz/notarealtree/airmart/AirMart.kt
+++ src/main/kotlin/xyz/notarealtree/airmart/AirMart.kt
@@ -12,6 +12,13 @@ import xyz.notarealtree.airmart.resource.FittingsResource
 import xyz.notarealtree.airmart.service.ApiProxy
 import xyz.notarealtree.airmart.service.DbManager
 import xyz.notarealtree.airmart.service.FittingManager
+import org.eclipse.jetty.servlets.CrossOriginFilter
+import javax.servlet.DispatcherType
+import java.util.EnumSet
+
+
+
+
 
 class AirMart: Application<AirMartConfiguration>() {
     companion object {
@@ -31,6 +38,15 @@ class AirMart: Application<AirMartConfiguration>() {
         environment?.jersey()?.register(FittingsResource(fittingsManager))
 
         environment?.healthChecks()?.register("redis", RedisHealthCheck(redisClient(configuration)))
+
+        val cors = environment?.servlets()?.addFilter("CORS", CrossOriginFilter::class.java)
+        cors?.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*")
+        cors?.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization")
+        cors?.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD")
+        cors?.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true")
+
+        // Add URL mapping
+        cors?.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType::class.java), true, "/*")
     }
 
     // TODO: check whether this has problems with concurrent calls
